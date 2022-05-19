f2271f15a35e48e2c59678d6a9991f55c8d8fa5b
indywip <indira.sowy@gmail.com>
2022-05-19 12:36:36 -0700

    changed heading

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,8 @@
 const AboutPage = () => {
   return (
     <>
-      <h3>A Little Bit About Me</h3>
-      <h5>I'm a first-year student at the University of British Columbia who designs and codes websites to solve problems and inspire others. </h5>
+      <h2>A Little Bit About Me</h2>
+      <h3>I'm a first-year student at the University of British Columbia who designs and codes websites to solve problems and inspire others. </h3>
       <p>As someone who’s been doing design since high school, creation is a passion close to my heart. I hope to use my skills to help develop the future of the tech industry.</p>
     </>
   );
