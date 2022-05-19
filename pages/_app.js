be88f718e4eea5bad2700c85b9542051724286c9
indywip <indira.sowy@gmail.com>
2022-05-19 12:16:31 -0700

    changed site name

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <Layout>
-      <Head title={`Nitin Ranganath | ${pageProps.title}`} />
+      <Head title={`Indy Sowy | ${pageProps.title}`} />
       <Component {...pageProps} />
     </Layout>
   );
