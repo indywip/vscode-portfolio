bca526354a67197fb096b97aa8af9ae2327af164
indywip <indira.sowy@gmail.com>
2022-05-19 12:33:07 -0700

    added to title

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,7 +2,8 @@ const AboutPage = () => {
   return (
     <>
       <h3>A Little Bit About Me</h3>
-      <p>'m a first-year student at the University of British Columbia who designs and codes websites to solve problems and inspire others. As someone who’s been doing design since high school, creation is a passion close to my heart. I hope to use my skills to help develop the future of the tech industry.</p>
+      <h5>I'm a first-year student at the University of British Columbia who designs and codes websites to solve problems and inspire others. </h5>
+      <p>As someone who’s been doing design since high school, creation is a passion close to my heart. I hope to use my skills to help develop the future of the tech industry.</p>
     </>
   );
 };
