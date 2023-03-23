af2a6d688298b3fded68985ec170e0084b970384
Indy Sowy <indira.sowy@gmail.com>
2023-03-22 17:33:17 -0700

    fixed experiences

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,13 +7,11 @@ const AboutPage = () => {
       <br></br>
       <h3>My Past Experiences</h3>
       <ul>
-      <li><strong>Operations Engineer</strong> @ UBC CEDaR Space Lab / <i>Oct. 2022 - Present</i></li>
+        <li><strong>Operations Engineer</strong> @ UBC CEDaR Space Lab / <i>Oct. 2022 - Present</i></li>
         <li><strong>Software Developer</strong> @ The Creative Solution / <i>Aug. 2022 - Dec. 2022</i></li>
-        <li><strong>Developer</strong> @ Leftindust / <i>Sep. 2022 - Nov. 2022</i></li>
         <li><strong>Prep Fellow</strong> @ Major League Hacking / <i>Jul. 2022</i></li>
         <li><strong>Web Developer</strong> @ UBC eProjects / <i>May 2022 - Present</i></li>
         <li><strong>Web Development Intern</strong> @ The Coding Foundation / <i>Feb. - May 2022</i></li>
-        <li><strong>Developer and Designer</strong> @ Elevate Tech / <i>Dec. 2021 - Feb. 2022</i></li>
       </ul>
     </>
   );
