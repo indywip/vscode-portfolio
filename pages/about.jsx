c47349a314833c874994a74c50c8696749d086d5
Indy Sowy <indira.sowy@gmail.com>
2023-12-04 15:38:43 -0800

    update projects

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,7 +2,7 @@ const AboutPage = () => {
   return (
     <>
       <h2>Who Am I?</h2>
-      <h4>I'm a second-year student at the University of British Columbia who designs and codes websites to solve problems and inspire others. </h4>
+      <h4>I'm a third-year student at the University of British Columbia who designs and codes websites to solve problems and inspire others. </h4>
       <p>As someone who’s been doing design since high school, creation is a passion close to my heart. I hope to use my skills to help develop the future of the tech industry.</p>
       <br></br>
       <h3>My Past Experiences</h3>
@@ -12,6 +12,7 @@ const AboutPage = () => {
         <li><strong>Software Developer</strong> @ The Creative Solution / <i>Aug. - Dec. 2022</i></li>
         <li><strong>Software Engineer Intern</strong> @ Dashlabs.ai (YC W21) / <i>Jun. - Aug. 2022</i></li>
         <li><strong>Prep Fellow</strong> @ Major League Hacking / <i>Jul. 2022</i></li>
+        <li><strong>UX Designer</strong> @ UBC Faculty of Applied Science / <i>May - Aug. 2022</i></li>
       </ul>
     </>
   );
