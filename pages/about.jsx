ac4abdb2f0c32a4c9cf0c833e4ea4c6ffbf4932f
Indy Sowy <indira.sowy@gmail.com>
2024-03-26 23:29:49 -0700

    update about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,11 +2,12 @@ const AboutPage = () => {
   return (
     <>
       <h2>Who Am I?</h2>
-      <h4>I'm a third-year student at the University of British Columbia who designs and codes websites to solve problems and inspire others. </h4>
-      <p>As someone who’s been doing design since high school, creation is a passion close to my heart. I hope to use my skills to help develop the future of the tech industry.</p>
+      <h4>I'm a third-year Cognitive Systems student at the University of British Columbia who designs and codes websites to solve problems and inspire others. </h4>
+      <p>As someone who’s been doing design since high school, creating / building products is a passion close to my heart. I hope to use my skills to help develop the future of the tech industry.</p>
       <br></br>
-      <h3>My Past Experiences</h3>
+      <h3>My Relevant Experiences</h3>
       <ul>
+        <li><strong>Software Engineer Intern</strong> @ Salesforce / <i>Incoming May 2024</i></li>
         <li><strong>Software Developer Intern</strong> @ Copperleaf Technologies / <i>May - Dec. 2023</i></li>
         <li><strong>Operations Engineer</strong> @ UBC CEDaR Space Lab / <i>Oct. - Apr. 2022</i></li>
         <li><strong>Software Developer</strong> @ The Creative Solution / <i>Aug. - Dec. 2022</i></li>
